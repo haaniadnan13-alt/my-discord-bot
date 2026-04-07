769a532d798e598a8c6c69bd83301522efc7ceda
haaniadnan13-alt <haaniadnan13@gmail.com>
2026-04-07 12:40:10 +0400

    Create server_new.js

---
+++ server_new.js
@@ -0,0 +1,8 @@
+module.exports = {
+    rl: require('./templates/rl.js'),
+    mc: require('./templates/mc.js'),
+    cod: require('./templates/cod.js'),
+    apex: require('./templates/apex.js'),
+    roblox: require('./templates/roblox.js'),
+    rivals: require('./templates/rivals.js')
+};
