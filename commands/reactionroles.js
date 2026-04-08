57f52ebc378d5f1c010a5b40807bee3302ca6b7b
haaniadnan13-alt <haaniadnan13@gmail.com>
2026-04-08 09:00:21 +0400

    Update reactionroles.js

--- commands/reactionroles.js
+++ commands/reactionroles.js
@@ -1,1 +1,16 @@
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
 
+module.exports = [{
+    data: new SlashCommandBuilder()
+        .setName('rr')
+        .setDescription('🎭 Manage reaction roles')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .addSubcommand(s => s.setName('add').setDescription('Add a role').addRoleOption(o => o.setName('role').setRequired(true).setDescription('The role')).addStringOption(o => o.setName('emoji').setRequired(true).setDescription('The emoji')))
+        .addSubcommand(s => s.setName('remove').setDescription('Remove a role').addStringOption(o => o.setName('emoji').setRequired(true).setDescription('The emoji')))
+        .addSubcommand(s => s.setName('list').setDescription('See all active reaction roles'))
+        .addSubcommand(s => s.setName('clear').setDescription('Delete all reaction roles in a channel')),
+    async execute(i) {
+        const sub = i.options.getSubcommand();
+        await i.reply({ content: `✅ Reaction Role **${sub}** executed!`, ephemeral: true });
+    }
+}];
