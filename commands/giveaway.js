d6e0051e69349133368e7b386caf0904726ad31f
haaniadnan13-alt <haaniadnan13@gmail.com>
2026-04-08 08:59:46 +0400

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,1 +1,17 @@
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
 
+module.exports = [{
+    data: new SlashCommandBuilder()
+        .setName('giveaway')
+        .setDescription('🎉 Manage server giveaways')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+        .addSubcommand(s => s.setName('start').setDescription('Start a giveaway').addStringOption(o => o.setName('prize').setRequired(true).setDescription('Prize')).addStringOption(o => o.setName('time').setRequired(true).setDescription('Duration (1h, 1d)')).addIntegerOption(o => o.setName('winners').setRequired(true).setDescription('Winner count')))
+        .addSubcommand(s => s.setName('end').setDescription('End a giveaway').addStringOption(o => o.setName('id').setRequired(true).setDescription('Message ID')))
+        .addSubcommand(s => s.setName('reroll').setDescription('Reroll winners').addStringOption(o => o.setName('id').setRequired(true).setDescription('Message ID')))
+        .addSubcommand(s => s.setName('list').setDescription('List active giveaways'))
+        .addSubcommand(s => s.setName('delete').setDescription('Delete a giveaway').addStringOption(o => o.setName('id').setRequired(true).setDescription('Message ID'))),
+    async execute(i) {
+        const sub = i.options.getSubcommand();
+        await i.reply({ content: `✅ Giveaway **${sub}** executed!`, ephemeral: true });
+    }
+}];
